2e860bfcb25304ac8a8a16a5aea3fb9b80b06783
Ryan Roberts <hello@ryanroberts.co.uk>
2022-11-08 10:11:03 +0000

    Correctly configure rss plugin and html date

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
+
+  eleventyConfig.addPlugin(pluginRss);
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
 
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
