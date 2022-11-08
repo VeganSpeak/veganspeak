47a22d834e7a9be745ffe2b9a3ab07a872ebf7bd
Ryan Roberts <hello@ryanroberts.co.uk>
2022-11-08 10:20:34 +0000

    Add filter for tag lists

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
+
   return {
     dir: {
       input: 'src',
